c92bcbb9455b98a86ecc0e8653e2bb72fc418741
loxmopharma-lab <loxmopharma@gmail.com>
2025-09-28 09:43:55 +0530

    Update customer.py

--- customer.py
+++ customer.py
@@ -1,1 +1,16 @@
+customers = []
 
+def add_customer(name, email):
+    customer = {
+        "name": name,
+        "email": email
+    }
+    customers.append(customer)
+
+def list_customers():
+    if not customers:
+        print("No customers found.")
+    else:
+        print("Customer List:")
+        for idx, customer in enumerate(customers, 1):
+            print(f"{idx}. {customer['name']} ({customer['email']})")
